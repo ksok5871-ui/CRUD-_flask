d23e810bcb3c347e6f410668705001519c490b3f
ksok5871-ui <ksok5871@gmail.com>
2026-09-10 22:08:03 +0700

    Update Code

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask,render_template
 
 app=Flask(__name__)
+#category
+@app.route('/category')
+def category():
+    return render_template('category/index.html')
 
 @app.route('/product')
 def index_product():
